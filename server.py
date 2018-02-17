2a9798d238efd5734f68f782b36e7e51b7305a13
Ed Powley <edpowley@gmail.com>
2018-02-17 21:16:12 +0000

    Implementing webhooks

--- server.py
+++ server.py
@@ -1,6 +1,101 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+import pymongo
+import subprocess
+
+from worker import WorkerThread
+
+# Change these if necessary
+JAVA_HOME = r"C:\Program Files\Java\jdk1.8.0_161"
+ANT_HOME = os.path.abspath("..\..\apache-ant-1.10.1")
+
+os.environ["JAVA_HOME"] = JAVA_HOME
+os.environ["ANT_HOME"] = ANT_HOME
+ANT_BAT = os.path.join(ANT_HOME, "bin", "ant.bat")
+
+app = flask.Flask(__name__)
+
+client = pymongo.MongoClient()
+db = client.comp250
+
+worker = WorkerThread()
+worker.daemon = True
+worker.start()
+
+def run_commands(working_dir, commands):
+	output = []
+	
+	for command in commands:
+		output.append('-' * 80)
+		output.append(working_dir + '> ' + ' '.join(command))
+		
+		result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8", cwd=working_dir)
+		output.append(result.stdout)
+		
+		if result.returncode != 0:
+			output.append('-' * 80)
+			output.append("Failed with error code {}".format(result.returncode))
+			return (False, '\n'.join(output))
+	
+	return (True, '\n'.join(output))
+	
+
+def pull_and_build(bot):
+	clone_path = os.path.join("..", "tournament", bot["_id"].replace("/", "+"))
+	commands = []
+	
+	if not os.path.exists(clone_path):
+		os.mkdir(clone_path)
+		commands = [
+			["git", "init"],
+			["git", "remote", "add", "origin", bot["repository"]["clone_url"]]
+		]
+	else:
+		commands = [
+			["git", "clean", "-fdx"],
+		]
+	
+	commands += [
+		["git", "fetch"],
+		["git", "reset", "--hard", bot["head"]],
+		["git", "submodule", "update"],
+		[ANT_BAT, "clean", "build", "jar"]
+	]
+
+	success, build_log = run_commands(clone_path, git_commands)
+	
+	status = "ready" if success else "error"
+	
+	db.bots.update_one(
+		{"_id": bot["_id"]},
+		{"$set": {
+			"status": status,
+			"build_log": build_log
+		}
+	})
+
+	return success
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	print("hhmm")
+	return "Hello World!"
+
+@app.route("/hook", methods=['POST'])
+def hook():
+	json = Flask.request.get_json()
+	
+	repo_name = json["repository"]["full_name"]
+	existing_bot = db.bots.find_one({"_id": repo_name})
+	
+	if existing_bot is None:
+		existing_bot = {"_id": repo_name}
+	
+	existing_bot["repository"] = json["repository"]
+	existing_bot["head"] = json["after"],
+	existing_bot["status"] = "building"
+	
+	db.bots.replace_one({"_id": repo_name}, existing_bot, upsert=True)
+	
+	worker.enqueue(pull_and_build, existing_bot)
+	
+	return ""
---
+++ worker.py
@@ -0,0 +1,26 @@
+import threading
+import queue
+
+""" A single thread which executes a queue of functions """
+class WorkerThread(threading.Thread):
+	class QueueItem:
+		def __init__(self, callable, args, kwargs):
+			self.callable = callable
+			self.args = args
+			self.kwargs = kwargs
+		
+		def run(self):
+			self.callable(*self.args, **self.kwargs)
+			
+	def __init__(self):
+		self.queue = queue.Queue()
+	
+	def enqueue(self, callable, *args, **kwargs):
+		item = QueueItem(callable, args, kwargs)
+		self.queue.put(item)
+	
+	def run(self):
+		while True:
+			item = self.queue.get() # Blocks until an item is available
+			item.run()
+
