aff24541030fe97122ec0162570b4a9a8c575670
Ed Powley <edpowley@gmail.com>
2018-02-22 23:07:56 +0000

    Tweaked leaderboard code

--- server.py
+++ server.py
@@ -9,9 +9,6 @@ app = flask.Flask(__name__)
 def leaderboard():
 	stats = list(db.stats.find({}, sort=[("elo", pymongo.DESCENDING)]))
 	
-	for stat in stats:
-		stat["author"], stat["repo_name"], stat["class_name"] = stat["_id"].split('+')
-	
 	return flask.render_template("index.html", stats=stats)
 
 # Import other app routes
