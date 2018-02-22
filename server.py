1c1b40ac4aa374d8b44ffb24181e85f3aa852631
Ed Powley <edpowley@gmail.com>
2018-02-22 22:58:03 +0000

    Added basic leaderboard page

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 import flask
+import pymongo
+
+from db import db
 
 app = flask.Flask(__name__)
 
 @app.route("/")
-def hello():
-	return "Hello World!"
+def leaderboard():
+	stats = list(db.stats.find({}, sort=[("elo", pymongo.DESCENDING)]))
+	
+	for stat in stats:
+		stat["author"], stat["repo_name"], stat["class_name"] = stat["_id"].split('+')
+	
+	return flask.render_template("index.html", stats=stats)
 
+# Import other app routes
 import webhook
 
